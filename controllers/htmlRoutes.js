c1b25be314cc1c45159b153e97b11884fefc57bd
Saydeezr <saydeezr@gmail.com>
2024-06-27 15:13:03 -0400

    added auth helper and incorporated into html routes along with data

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,15 +1,45 @@
 const router = require('express').Router();
 const { Model } = require('sequelize');
-const { Project, User } = require('../models');
+const { Blog, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 //directs to homepage
-router.get('/', (req,res) => {
-    res.render('homepage')
+router.get('/', async (req,res) => {
+    try{
+        const blogData = await Blog.findAll({
+            include: [{
+                model: User,
+                attributes: ['name'],
+            },
+        ],
+        });
+
+        const blogs = blogData((blogs) => blogData.get({plain: true}));
+        res.render('homepage', {
+            blogs,
+            logged_in: req.session.logged_in
+        });
+    } catch(err) {
+        res.status(500).json(err);
+    }
 });
   
 // directs to dashboard
-router.get('/dashboard', (req,res) => {
-    res.render('dashboard')
+router.get('/dashboard', withAuth, async (req,res) => {
+    try{ 
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Blog }],
+        });
+
+        const user = userData.get({ plain: true });
+        res.render('dashboard', { 
+            ...user,
+            logged_in: true
+        });
+    } catch(err) {
+        res.status(500).json(err);
+    }
 });
 
 // directs signup page
